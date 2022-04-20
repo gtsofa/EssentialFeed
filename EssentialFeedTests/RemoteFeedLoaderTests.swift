0d55b306fb8f6ae6d9e660898b4ee62ef24a4d7e
gtsofa <jlsnyule@yahoo.com>
2022-04-20 18:19:07 +0300

    Guarantee that we can call more than once

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -19,13 +19,26 @@ class RemoteFeedLoaderTests: XCTestCase {
         
     }
     
-    func test_load_requestDataFromURL() {
+    func test_load_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
+        
+    }
+    
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        
+       
+        XCTAssertEqual(client.requestedURLs, [url, url])
         
     }
     
@@ -40,8 +53,12 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
+        var requestedURLs = [URL]()
+        
+        
         func get(from url: URL) {
             requestedURL = url
+            requestedURLs.append(url)
         
         }
     }
