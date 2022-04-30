05797d919a735f2f88767040e14d74dd4bcb32ba
gtsofa <jlsnyule@yahoo.com>
2022-04-30 13:20:15 +0300

    Moved mapping logic to a new type `FeedItemsMapper`

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -39,8 +39,8 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                if response.statusCode == 200, let root = try?   JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items.map({$0.item})))
+                if let items = try? FeedItemsMapper.map(data, response) {
+                    completion(.success(items))
                 } else {
                     completion(.failure(.invalidData))
                 }
@@ -50,6 +50,15 @@ public final class RemoteFeedLoader {
             }
             
         }
+    }
+}
+
+private class FeedItemsMapper {
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        return try JSONDecoder().decode(Root.self, from: data).items.map {$0.item }
     }
 }
 
