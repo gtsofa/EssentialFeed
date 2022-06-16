a884a41ebbc791a22442b90964f50f20ba2b2c8f
gtsofa <jlsnyule@yahoo.com>
2022-06-17 00:48:24 +0300

    Insert delivers error on insertion error (invalid store URL) -2-

--- EssentialFeedTests/Feed Cache/CodableFeedStoreCloneTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreCloneTests.swift
@@ -59,11 +59,15 @@ class CoddableFeedStoreClone {
     }
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
-        let encoder = JSONEncoder()
-        let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
-        let encoded = try! encoder.encode(cache)
-        try! encoded.write(to: storeURL)
-        completion(nil)
+        do {
+            let encoder = JSONEncoder()
+            let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
+            let encoded = try encoder.encode(cache)
+            try encoded.write(to: storeURL)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
         
     }
 }
@@ -148,6 +152,18 @@ class CodableFeedStoreCloneTests: XCTestCase {
         XCTAssertNil(latestInsertionError, "Expected to override cache successfully")
         
         expect(sut, toRetrieve: .found(feed: latestFeed, timestamp: latestTimestamp))
+    }
+    
+    func test_insert_deliversErrorOnInsertionError(){
+        
+        let invalidStoreURL = URL(string: "invalid://store-url")
+        let sut = makeSUT(storeURL: invalidStoreURL)
+        let feed = uniqueItems().local
+        let timestamp = Date()
+        
+        let insertionError = insert((feed, timestamp), to: sut)
+        XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error")
+
     }
     
     // MARK: - Helpers
