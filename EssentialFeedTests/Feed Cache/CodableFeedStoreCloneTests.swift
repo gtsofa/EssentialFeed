e1f88f84cc0757bbb08b9ce64bdfa40be53a7a73
gtsofa <jlsnyule@yahoo.com>
2022-06-17 17:55:38 +0300

    Create `FeedStoreSpecs` that must be implemented by any `FeedStore` test implementation to guarantee it meets spec

--- EssentialFeedTests/Feed Cache/CodableFeedStoreCloneTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreCloneTests.swift
@@ -8,45 +8,11 @@
 import XCTest
 import EssentialFeed
 
-protocol FeedStoreCloneSpecs {
-    func test_retrieve_deliversEmptyOnEmptyCache()
-    func test_retrieve_hasNoSideEffectsOnEmptyCache()
-    func test_retrieve_deliversFoundValuesOnNonEmptyCache()
-    func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
 
-    func test_insert_deliversNoErrorOnEmptyCache()
-    func test_insert_deliversNoErrorOnNonEmptyCache()
-    func test_insert_overridesPreviouslyInsertedCacheValues()
-    
-    func test_delete_deliversNoErrorOnEmptyCache()
-    func test_delete_hasNoSideEffectsOnNonEmptyCache()
-    func test_delete_deliversNoErrorOnNonEmptyCache()
-    func test_delete_emptiesPreviouslyInsertedCache()
-
-   
-
-    func test_storeSideEffects_runSerially()
-}
-
-protocol FailableRetrieveFeedStoreCloneSpecs {
-    func test_retrieve_deliversFailureOnRetrievalError()
-    func test_retrieve_hasNoSideEffectsOnFailure()
-    
-}
-
-protocol FailableInsertFeedStoreCloneSpecs {
-    func test_insert_deliversErrorOnInsertionError()
-    func test_insert_hasNoSideEffectsOnInsertionError()
-    
-}
-
-protocol FailableDeleteFeedStoreCloneSpecs {
-    func test_delete_deliversErrorOnDeletionError()
-    func test_delete_hasNoSideEffectsOnDeletionError()
-}
+typealias FailableFeedStoreCloneSpecs = FailableRetrieveFeedStoreSpecs & FailableInsertFeedStoreSpecs & FailableDeleteFeedStoreSpecs
 
 
-class CodableFeedStoreCloneTests: XCTestCase {
+class CodableFeedStoreCloneTests: XCTestCase, FailableFeedStoreCloneSpecs {
     
     override func tearDown() {
         super.tearDown()
