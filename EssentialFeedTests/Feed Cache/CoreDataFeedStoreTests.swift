3730eef9a4ac6e88b5fc3d2a1e456f328823d3c0
gtsofa <jlsnyule@yahoo.com>
2022-06-19 20:03:44 +0300

    `CoreDataFeedStore.deleteCachedFeed()` empties previously inserted cache

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -75,7 +75,9 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_delete_emptiesPreviouslyInsertedCache() {
+        let sut = makeSUT()
         
+        assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
     }
     
     func test_storeSideEffects_runSerially() {
