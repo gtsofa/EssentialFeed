62354a219c39c7f7f8be8a162d97c390fa2e2e0c
gtsofa <jlsnyule@yahoo.com>
2022-07-28 12:47:44 +0300

    Add prototype storyboard with table view and feed cell layout

---
+++ PrototypeII/PrototypeII/FeedViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FeedViewController.swift
+//  PrototypeII
+//
+//  Created by Julius on 28/07/2022.
+//
+
+import UIKit
+
+class FeedViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        return 0
+//    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "FeedImageCell")!
+    }
+
+
+}
