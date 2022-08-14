d79b0c83a86fc59e54c00b171254e4f5eedb4acd
gtsofa <jlsnyule@yahoo.com>
2022-08-15 00:05:25 +0300

    Add type aliases to shorten type definitions with generics

--- EssentialApp/EssentialApp/FeedUIComposer.swift
+++ EssentialApp/EssentialApp/FeedUIComposer.swift
@@ -12,10 +12,12 @@ import EssentialFeediOS
 public final class FeedUIComposer {
     private init() {}
     
+    private typealias FeedPresentationAdapter = LoadResourcePresentationAdapter<[FeedItem], FeedViewAdapter>
+    
     public static func feedComposedWith(
         feedLoader: @escaping () -> AnyPublisher<[FeedItem], Error>,
         imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) -> FeedViewController {
-            let presentationAdapter = LoadResourcePresentationAdapter<[FeedItem], FeedViewAdapter>(loader: feedLoader)
+            let presentationAdapter = FeedPresentationAdapter(loader: feedLoader)
         
         let feedController = makeFeedViewController(delegate: presentationAdapter, title: FeedPresenter.title)
         presentationAdapter.presenter = LoadResourcePresenter(
