8fe11c8f415b9c97086cfcf68db3ce2eb231c7b3
gtsofa <jlsnyule@yahoo.com>
2022-08-24 15:13:36 +0300

    Remove unused code

--- EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -14,14 +14,6 @@ class FeedPresenterTests: XCTestCase {
     func test_title_isLocalized() {
         XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
     }
-    
-    func test_map_createsViewModel() {
-        let feed = uniqueItems().models
-        
-        let viewModel = FeedPresenter.map(feed)
-        
-        XCTAssertEqual(viewModel.feed, feed)
-    }
 
     // MARK: - Helpers
     
